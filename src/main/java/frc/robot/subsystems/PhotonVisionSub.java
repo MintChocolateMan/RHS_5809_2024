b1292d0d941dc5d1fdb2a4a2cbabf654b3bbddd3
MintChocolateMan <eliasmanis05@gmail.com>
2024-02-01 16:23:12 -0600

    added sad apriltag

---
+++ src/main/java/frc/robot/subsystems/PhotonVisionSub.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import org.photonvision.PhotonUtils;
+import org.photonvision.common.hardware.*;
+
+public class PhotonVisionSub extends SubsystemBase {
+
+    PhotonCamera shooterCam;
+
+    public PhotonVisionSub() {
+
+        shooterCam =
+    }
+    
+}
