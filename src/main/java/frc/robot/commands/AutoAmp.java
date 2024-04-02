c0c4d9585a2207257e01bcaf39d0132f96ee451d
Eli <eliasmanis@amdg.rockhursths.edu>
2024-04-01 21:01:11 -0500

    happy little change

--- src/main/java/frc/robot/commands/AutoAmp.java
+++ src/main/java/frc/robot/commands/AutoAmp.java
@@ -4,7 +4,6 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
