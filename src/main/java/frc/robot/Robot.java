b3e124491223ace4e02967f4dcff1113f428ea21
MintChocolateMan <eliasmanis05@gmail.com>
2024-04-04 09:23:58 -0500

    added portforwarding for LLs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -28,6 +29,19 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // Port Forwarding for Limelights
+    for (int port = 5800; port <= 5809; port++) {
+      PortForwarder.add(port, "limelight-shooter.local", port);
+    }
+
+    for (int port = 5800; port <= 5809; port++) {
+      PortForwarder.add(port, "limelight-intake.local", port);
+    }
+
+    for (int port = 5800; port <= 5809; port++) {
+      PortForwarder.add(port, "limelight-amp.local", port);
+    }
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
